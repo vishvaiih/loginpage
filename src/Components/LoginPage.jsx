deb8a4e5e444487d90e026ff09be7aab8e4ea1ea
vishvaiih <vishva.iihglobal@gmail.com>
2025-11-10 11:54:17 +0530

    complete formik
    complete formik

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,13 +1,34 @@
+import { useFormik } from "formik";
 import React from "react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-const SignupSchema = Yup.object().shape({
-  usrname: Yup.string().required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-});
 
 
 function LoginPage() {
+  const signupSchema = Yup.object().shape({
+    Username: Yup.string().required("Username is Required"),
+    website: Yup.string()
+      .url("Invalid URL formate")
+      .required("website is Required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is Required"),
+    password: Yup.string()
+      .min(8, "passwor must be at least 8 character")
+      .required("Password is Required"),
+    ConfirmPassword: Yup.string()
+      .oneOf([Yup.ref("Password"), null], "Passwords must match")
+      .required("ConfirmPassword is Required"),
+  });
+  
+  const formik = useFormik({
+    initialValues:{
+      username:"",
+      // website:"",
+    }
+  });
+  
+
   return (
     <>
       <div class="main">
@@ -38,12 +59,14 @@ function LoginPage() {
             <h4>Join Us Today</h4>
             <p>Enter your email and password to register</p>
           </div>
-         
 
-         
-           <div class="form-floating mb-3">
+          <div class="form-floating mb-3">
             <div>
               <input
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                error={formik.touched.username && Boolean(formik.errors.username)}
+                helperText={formik.touched.username && formik.errors.username}
                 type="text"
                 class="form-control"
                 id="floatingInput"
@@ -58,6 +81,10 @@ function LoginPage() {
 
             <div>
               <input
+                value={formik.values.website}
+                onChange={formik.handleChange}
+                error={formik.touched.website && Boolean(formik.errors.website)}
+                helperText={formik.touched.website && formik.errors.website}
                 type="text"
                 class="form-control"
                 id="floatingInput"
@@ -68,6 +95,10 @@ function LoginPage() {
 
             <div>
               <input
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
                 type="email"
                 class="form-control"
                 id="floatingInput"
@@ -78,6 +109,10 @@ function LoginPage() {
 
             <div>
               <input
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
                 type="password"
                 class="form-control"
                 id="floatingInput"
@@ -88,6 +123,10 @@ function LoginPage() {
 
             <div>
               <input
+              value={formik.values.ConfirmPassword}
+              onChange={formik.handleChange}
+              error={formik.touched.ConfirmPassword && Boolean(formik.errors.ConfirmPassword)}
+              helperText={formik.touched.ConfirmPassword && formik.errors.ConfirmPassword}
                 type="password"
                 class="form-control"
                 id="floatingInput"
@@ -96,15 +135,17 @@ function LoginPage() {
               />
             </div>
           </div>
-         
 
-      
-
-          
-
-          <div class="form-check" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+          <div
+            class="form-check"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <input
-              style={{margin:" 20px "}}
+              style={{ margin: " 20px " }}
               class="form-check-input"
               type="checkbox"
               value=""
@@ -115,16 +156,22 @@ function LoginPage() {
             </label>
           </div>
 
-         <div style={{display:"flex",justifyContent:"center"}}>
-              <button class="btn btn-primary"  style={{width:"80%"}}>Sign In</button>
-         </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button class="btn btn-primary" style={{ width: "80%" }}>
+              Sign In
+            </button>
+          </div>
 
-         <div style={{display:"flex",justifyContent:"center",margin:"20px"}}>
-              <p>You have an account?</p>
-              <p style={{color:"blue"}}>Sign In </p>
-         </div>
-        
-        
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "20px",
+            }}
+          >
+            <p>You have an account?</p>
+            <p style={{ color: "blue" }}>Sign In </p>
+          </div>
         </div>
       </div>
     </>
